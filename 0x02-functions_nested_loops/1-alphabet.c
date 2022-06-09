811fcc654530f6021e07457188223a311fe5bcdf
jyanokye <jamesyawanokye17@gmail.com>
2022-06-08 18:45:27 -0700

    do

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,8 @@
  * print_alphabet - Print alphabet in lowercase using only '_putchar'
  * Return: 0
  */
-void print_alphabet(void)
+void print_alphabet(void);
+int main()
 {
 	char c = 'a';
 
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,8 @@
  * print_alphabet_x10 - print alphabet in lowercase 10 times
  * Return: 0
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
+int main()
 {
 	int i = 0;
         char c;
