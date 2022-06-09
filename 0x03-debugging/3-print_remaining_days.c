02ae71e13519e60f220d334cd1d5127f0e4c24fa
jyanokye <jamesyawanokye17@gmail.com>
2022-06-08 19:58:48 -0700

    debug

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -50,7 +50,7 @@ void print_remaining_days(int month, int day, int year)
 
 			    {
 
-				            if (month == 2 && day == 60)
+				            if (month == 2 && day > 60)
 
 						            {
 
