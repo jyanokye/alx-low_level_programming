f9b2d6f6f01f16fcdfd358040356771ceb210856
jyanokye <jamesyawanokye17@gmail.com>
2022-11-21 14:18:24 -0800

    kay

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - main block
+ * This code prints all single digit numbers of base ten starting from 0 to 9
+ * Return: 0
+ */
+
+int main(void)
+{
+int sin = 0;
+while (sin < 10)
+{
+printf("%d", sin);
+sin++;
+}
+printf("\n");
+return (0);
+
+}
