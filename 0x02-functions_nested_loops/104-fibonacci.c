2499e10250c99da2d3d0c7fb2cddafcbaa353c22
jyanokye <101642075+jyanokye@users.noreply.github.com>
2023-01-11 09:18:45 +0000

    Create 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the first 98 fibonacci numbers starting from 1 and 2
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int n;
+	unsigned long int m = 1;
+	unsigned long int o = 0;
+	unsigned long int a;
+
+	for (n = 0; n < 98; n++)
+	{
+		a = m + o;
+
+		printf("%lu", a);
+
+		if (n < 97)
+		{
+			printf(",");
+			printf(" ");
+		}
+		o = m;
+		m = a;
+	}
+	printf("\n");
+	return (0);
+}
