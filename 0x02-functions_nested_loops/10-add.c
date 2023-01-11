48d928720bf2b49f4d61a162ddf80f86c886232c
jyanokye <101642075+jyanokye@users.noreply.github.com>
2023-01-11 09:24:26 +0000

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: the first argument
+ * @b: second argument
+ *Return: Always 0
+ */
+
+int add(int g, int h)
+{
+	int k;
+
+	k = g + h;
+	return (k);
+}
