e412436ff0e0acb912ee36ae26eb98819754d1d5
jyanokye <101642075+jyanokye@users.noreply.github.com>
2023-01-29 14:51:59 +0000

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/o https://raw.github.com/jyanokye/alx-low_level_programming/main/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp/o https://raw.github.com/jyanokye/alx-low_level_programming/main/0x18-dynamic_libraries/random.so
+export LD_PRELOAD=/tmp/random.so
